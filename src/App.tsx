f6e7bb60c41c3471c12a4e125d511768dac56387
kellz <esquejokelly@gmail.com>
2024-05-30 12:17:54 -0700

    Added required features (i.e. search, filtering, and toggle invisibilty

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import "ag-grid-community/styles/ag-theme-quartz.css";
 import { StyledInput, StyledSelect } from "./styles";
 import rowData from "./data.json";
 
+import "./App.css";
+
 type Row = {
   id: number;
   first_name: string;
@@ -16,21 +18,33 @@ type Row = {
 
 function App() {
   const [search, setSearch] = useState<string>("");
-  const [selectedRowID, setSelectedRowID] = useState(false);
-
+  const [selectedRowID, setSelectedRowID] = useState<number | null>(null);
+  
   // filtering should happen from the values in rowData, use the option to filter the desired column based on the user search.
   // colDefs should be dynamic, same work you do to the options can be done to it.
   // to style the input, you can just pass a prop similar
 
+  // Changed header with headerName instead of field 
   const columnDefs = [
-    { field: "id" },
-    { field: "first_name" },
-    { field: "last_name", onCellClicked: (e) => console.log("here", e) },
+    { headerName: "ID",
+      field: "id",
+      minWidth: 75,
+      maxWidth: 75,},
+    { headerName: "First Name",
+      field: "first_name",
+     },
+    { headerName: "Last Name",
+      field: "last_name",
+      
+      onCellClicked: (e) => console.log("here", e) },
     {
+      headerName: "IP Address",
       field: "ip_address",
+    
       cellRenderer: (params) => {
         const currentNodeId = params.node.id;
-        if (false) {
+        
+        if (selectedRowID === currentNodeId) {
           // in here you will check if you want to render the ip address or not.
           return params.value;
         }
@@ -39,10 +53,18 @@ function App() {
       onCellClicked: (params) => {
         const currentNodeId = params.node.id;
         console.log("current cell clicked", currentNodeId);
-        // setSelectedRowID();
+        if (params.column.colId === "ip_address") {
+          setSelectedRowID(selectedRowID === currentNodeId ? null : currentNodeId);
+        }  
       },
     },
-    { field: "balance", valueFormatter: (p) => "$" + p.value }, // in here you will finish formatting the balance,
+    { 
+      headerName: "Balance",
+      field: "balance",
+    
+      // Formats balance from 42517 to $42,517.00
+      // Adds two decimal places and replaces every thousand with a comma using regular expression
+      valueFormatter: (p) => "$" + p.value.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}, // in here you will finish formatting the balance,
   ] as const;
 
   const [selectedOption, setSelectedOption] =
@@ -59,14 +81,14 @@ function App() {
         const [label] = e;
 
         // filter the options to not have duplicate values
-
-        opt.push({
-          label,
-          value: label,
-        });
+          if (!opt.some(option => option.value === label)) {
+          opt.push({
+            label,
+            value: label,
+          });
+        }
       });
     });
-
     return opt;
   };
 
@@ -74,27 +96,70 @@ function App() {
     flex: 1,
   };
 
+  // Searches and filters the data by iterating through the selected row, 
+  // which was chosen through the drop down menu, and looks for the search string
+  const filteredRowData = rowData.filter((row) => {
+    if (!search) 
+      return true;
+
+    // Converts cells to lowercase for comparing
+    const value = row[selectedOption]?.toString().toLowerCase();
+
+    // Check if it matches with search
+    return value.includes(search.toLowerCase());
+  });
+  
+  const gridOptions= {
+    pagination: true,
+    rowSelection: 'multiple',
+    suppressDragLeaveHidesColumns: true,
+    enableCharts: true,
+    defaultColDef: defaultColDef,
+    rowData: filteredRowData,
+    columnDefs: columnDefs,
+  }
+
   return (
     <div
-      className="ag-theme-quartz"
       style={{
         height: 500,
       }}
+      className={"ag-theme-quartz-dark"}
     >
-      <StyledInput
-        $yourProp={true}
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+      <div className="header">
+        <picture>
+          <source srcSet="/ga-fav-icon.png" media="(max-width: 625px)" />
+          <source srcSet="/ga-logo-light.png" media="(max-width: 2560px)" />
+          <img src="/ga-logo-light.png" className="logo" alt="Logo" />
+        </picture>
+
+        <h1>KPI Dashboard</h1>
+      </div>
+
+      <div className="navbar-container"> 
+        <StyledSelect
+          options={options()}
+          value={selectedOption}
+          onChange={(e) =>
+            setSelectedOption(e as (typeof columnDefs)[number]["field"])
+          }
+        />
+        <StyledInput
+          $yourProp={false}
+          value={search}
+          placeholder="Search..."
+          onChange={(e) => {
+            console.log("Searching: ", e.target.value);
+            setSearch(e.target.value)}
+          }
+        />
+      </div>
+
+      <AgGridReact
+        gridOptions={gridOptions}
       />
-      <StyledSelect
-        options={options()}
-        value={selectedOption}
-        onChange={(e) =>
-          setSelectedOption(e as (typeof columnDefs)[number]["field"])
-        }
-      />
-      <AgGridReact defaultColDef={defaultColDef} rowData={[]} columnDefs={[]} />
     </div>
+  
   );
 }
 
--- src/styles.ts
+++ src/styles.ts
@@ -2,9 +2,15 @@ import styled from "styled-components";
 import { Input, Select } from "antd";
 
 export const StyledInput = styled(Input)<{ $yourProp?: boolean }>`
-  background-color: ${({ $yourProp }) => ($yourProp ? "salmon" : "cyan")};
+  background-color: ${({ $yourProp }) => ($yourProp ? "salmon" : "#1E90FF")};
+  width: 500px;
+  autoSize: false;
+  height: 30px;
 `;
 
+// Drop down menu 
 export const StyledSelect = styled(Select)`
-  min-width: 250px;
+  min-width: 100px;
+  height: 30px;
 `;
+
